efa01727be2609a2ac9623f9fa02fc253060e441
moon in learn <mooninlearn@gmail.com>
2023-05-01 18:35:06 +0900

    ~Section 26, 27(functions.php university_post_type())

--- app/public/wp-content/themes/fictional-university-theme/functions.php
+++ app/public/wp-content/themes/fictional-university-theme/functions.php
@@ -10,11 +10,20 @@ function university_files() {
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
-    register_nav_menu('headerMenuLocation', 'Header Menu Location');
-    register_nav_menu('footerLocationOne', 'Footer Location One');
-    register_nav_menu('footerLocationTwo', 'Footer Location Two');
     add_theme_support('title-tag');
 }
 
 add_action('after_setup_theme', 'university_features');
+
+function university_post_types() {
+    register_post_type('event', array(
+        'public' => true,
+        'labels' => array(
+            'name' => 'Events',
+        ),
+        'menu_icon' => 'dashicons-calendar'
+    ));
+}
+
+add_action('init', 'university_post_types');
 ?>
